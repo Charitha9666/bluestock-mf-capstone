ac66f7b97e221066fa799e719329a649f77067ef
Charitha9666 <gurramcharitha9666@gmail.com>
2026-06-02 20:35:07 +0530

    Day 2 SQL queries completed

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -37,3 +37,32 @@ nav_df = nav_df[nav_df["nav"] > 0]
 nav_df.to_csv("data/processed/02_nav_history_clean.csv", index=False)
 
 print("\nCleaned file saved successfully!")
+
+
+# Clean remaining files
+
+files = [
+    "01_fund_master.csv",
+    "03_aum_by_fund_house.csv",
+    "04_monthly_sip_inflows.csv",
+    "05_category_inflows.csv",
+    "06_industry_folio_count.csv",
+    "07_scheme_performance.csv",
+    "08_investor_transactions.csv",
+    "09_portfolio_holdings.csv",
+    "10_benchmark_indices.csv"
+]
+
+for file in files:
+    df = pd.read_csv(f"data/raw/{file}")
+
+    df = df.drop_duplicates()
+
+    output_file = file.replace(".csv", "_clean.csv")
+
+    df.to_csv(
+        f"data/processed/{output_file}",
+        index=False
+    )
+
+    print(output_file, "saved")
--- sql/queries.sql
+++ sql/queries.sql
@@ -1,13 +1,48 @@
-SELECT * FROM fact_nav LIMIT 10;
+
+SELECT * FROM fact_nav
+ORDER BY nav DESC
+LIMIT 5;
+
 
 SELECT AVG(nav) AS average_nav
 FROM fact_nav;
 
+
 SELECT MAX(nav) AS highest_nav
 FROM fact_nav;
+
 
 SELECT MIN(nav) AS lowest_nav
 FROM fact_nav;
 
+
 SELECT COUNT(*) AS total_records
 FROM fact_nav;
+
+
+SELECT amfi_code, AVG(nav) AS avg_nav
+FROM fact_nav
+GROUP BY amfi_code;
+
+
+SELECT MAX(date) AS latest_date
+FROM fact_nav;
+
+
+SELECT MIN(date) AS earliest_date
+FROM fact_nav;
+
+
+SELECT *
+FROM fact_nav
+WHERE nav > (
+    SELECT AVG(nav)
+    FROM fact_nav
+);
+
+SELECT amfi_code,
+       AVG(nav) AS avg_nav
+FROM fact_nav
+GROUP BY amfi_code
+ORDER BY avg_nav DESC
+LIMIT 10;
