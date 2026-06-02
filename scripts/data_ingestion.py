0610bb742b65917e052a37c35af12bd6f25bd164
Charitha9666 <gurramcharitha9666@gmail.com>
2026-06-02 12:06:57 +0530

    Day 1: Data ingestion complete

---
+++ scripts/data_ingestion.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+files = [
+    "01_fund_master.csv",
+    "02_nav_history.csv",
+    "03_aum_by_fund_house.csv",
+    "04_monthly_sip_inflows.csv",
+    "05_category_inflows.csv",
+    "06_industry_folio_count.csv",
+    "07_scheme_performance.csv",
+    "08_investor_transactions.csv",
+    "09_portfolio_holdings.csv",
+    "10_benchmark_indices.csv"
+]
+
+for file in files:
+    try:
+        df = pd.read_csv(f"data/raw/{file}")
+
+        print("\n" + "="*50)
+        print(file)
+        print("="*50)
+
+        print("Shape:", df.shape)
+        print("\nData Types:")
+        print(df.dtypes)
+
+        print("\nFirst 5 Rows:")
+        print(df.head())
+
+    except Exception as e:
+        print(f"Error reading {file}: {e}")
---
+++ scripts/live_nav_fetch.py
@@ -0,0 +1,19 @@
+import requests
+import pandas as pd
+
+url = "https://api.mfapi.in/mf/125497"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    nav_df = pd.DataFrame(data["data"])
+
+    nav_df.to_csv("data/raw/hdfc_top100_nav.csv", index=False)
+
+    print("NAV data saved successfully!")
+    print(nav_df.head())
+
+else:
+    print("Failed to fetch data")
